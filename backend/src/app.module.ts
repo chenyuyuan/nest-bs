03b058dd01d9cd06ed031dcf3eac5b931eed4d80
chenyuyuan <1695215206@qq.com>
2020-03-28 22:04:26 +0800

    backend cats

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogModule } from './blog/blog.module';
+import { CatsController } from './cats/cats.controller';
+
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://101.132.105.38/bs', { useNewUrlParser: true }),
     BlogModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '101.132.105.38',
+      port: 3306,
+      username: 'root',
+      password: 'Aaa1111.',
+      database: 'health_center',
+      entities: [],
+      synchronize: true,
+    }),
   ],
-  controllers: [AppController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as serveStatic from 'serve-static';
+import path = require('path');
 
 async function bootstrap() {
   const app = await NestFactory.create( AppModule);
-  app.enableCors();
+  // 使用serve-static
+  // '/public' 是路由名称，即你访问的路径为：host/public
+  // serveStatic 为 serve-static 导入的中间件，其中'../public' 为本项目相对于src目录的绝对地址
+  app.use('/public', serveStatic(path.join(__dirname, '../public'), {
+    maxAge: '1d',
+    extensions: ['jpg', 'jpeg', 'png', 'gif'],
+   }));
+
+  app.enableCors(); //允许跨域访问
   await app.listen(3000);
 }
 bootstrap();
