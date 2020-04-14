8d2c663675b3b02e3ca48b508b4b8fe325981dbb
chenyuyuan <1695215206@qq.com>
2020-04-15 01:34:09 +0800

    to hardware

--- hardware/teststr.py
+++ hardware/teststr.py
@@ -2,7 +2,7 @@
 import struct
 
 str_recv = "+CM2MCLIRECV: 0Bsdfcsfsdfsdf"  # recv success
-str_new = "+CM2MCLI: 1"  # new success
+str_new = "+CM2MCLI: 12\r\n"  # new success
 str_send = "+CM2MCLI: 5"  # send success
 str_error = "ERROR"
 
@@ -13,7 +13,7 @@ data_hex = data_hex[2:] if len(data_hex) % 2 == 0 else "0" + data_hex[2:]
 print(data_hex)
 
 # print(str_recv[0:14])
-# print(str_new[0:13])
+print(str_new[0:-2])
 # print(str_recv[14:])
 # print(time.time())
 # time.sleep(10)
