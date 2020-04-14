ab436174fa2511aea5e73198f70928a825234712
chenyuyuan <1695215206@qq.com>
2020-04-15 01:37:10 +0800

    to hardware

--- hardware/test.py
+++ hardware/test.py
@@ -22,7 +22,7 @@ def rcv_data(queue):
         else:
             if rcv[0:14] == "+CM2MCLIRECV: ":
                 queue.put(rcv[14:])
-                print("rcv_data: "+str(rcv[14:]))
+                print("rcv_data: "+str(rcv[14:-2]))
             if rcv[0:10] == "+CM2MCLI: ":
                 cm2mcli_code = rcv[10:-2]
                 print("+CM2MCLI code: " + str(cm2mcli_code))
