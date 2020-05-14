a9188638e80e7cac643f33642b3357f5e3e1cced
chenyuyuan <1695215206@qq.com>
2020-05-14 15:29:51 +0800

    img

--- frontend/src/components/post/PostList.vue
+++ frontend/src/components/post/PostList.vue
@@ -14,31 +14,30 @@
                 <div style="display:none" class="grid-content bg-purple">1</div>
             </el-col>
             <el-col :span="16">
-                <el-card class="box-card" shadow="hover" v-on:click="toPost">
+                <el-card class="box-card" shadow="hover" v-on:click="toPost" v-for="p1 in postList1" :key="p1.id">
                     <div slot="header" class="clearfix" style="">
                         <div class="form-inline">
                             <div>name</div>
                             <div>·</div>
-                            <div>time</div>
+                            <div>{{p1.time}}</div>
                         </div>
                     </div>
                     <div>
-                        title
+                        {{p1.title}}
                     </div>
                     <div>
-                        content
+                        {{p1.content}}
                     </div>
-                    <div>
-                        img
-                    </div>
+                    
+                    <el-image style="height:auto;width:300px" :fit="fill" v-if="p1.img != null" :src="p1.img"></el-image>
                     <div class="form-inline" style="margin-top:5px" v-on:click="toPost">
                         <i class="el-icon-arrow-up" style="color:blue;font-size:22px"></i>
                         <div style="width:8px">&nbsp;</div>
-                        <div style="color:blue;font-size:15px">1</div>
+                        <div style="color:blue;font-size:15px">{{p1.like}}</div>
                         <div style="width:24px">&nbsp;</div>
                         <i class="el-icon-chat-round" style="color:blue;font-size:22px"></i>
                         <div style="width:8px">&nbsp;</div>
-                        <div style="color:blue;font-size:15px">1</div>
+                        <div style="color:blue;font-size:15px">{{p1.comment}}</div>
                     </div>
                 </el-card>
             </el-col>
@@ -102,9 +101,7 @@
                     <div>
                         content
                     </div>
-                    <div>
-                        img
-                    </div>
+                    <img :src="item"/>
                     <div class="form-inline" style="margin-top:5px" v-on:click="toPost">
                         <i class="el-icon-arrow-up" style="color:blue;font-size:22px"></i>
                         <div style="width:8px">&nbsp;</div>
@@ -198,6 +195,10 @@ export default {
         value: '',
         label: ''
       },],
+      postList1:[],
+      postList2:[],
+      postList3:[],
+      postList4:[],
 
     }
   },
@@ -225,9 +226,42 @@ export default {
     },
   },
   mounted() {
-    axios.get(`${server.baseURL}/device/my_device`, ).then(data => {
-      console.log(data)
-      this.tableData = data.data.devices
+    axios.get(`${server.baseURL}/article/articlelist/0`, ).then(data => {
+      if(data.data.msg == "get_article_success") {
+        this.postList1 = data.data.articles
+        var pLen = this.postList1.length;
+        for (var i = 0;i < pLen; ++i) {
+          console.log(this.postList1[i]["img"])
+          if(this.postList1[i]["img"] == null || this.postList1[i]["img"] == "") {
+            this.postList1[i]["img"] == null
+          }
+          else {
+            this.postList1[i]["img"] = server.baseURL+"/public/upload/"+this.postList1[i]["author_id"]+"/"+this.postList1[i]["img"]
+            console.log(this.postList1[i]["img"])
+            console.log("            ")
+          }
+
+        }
+
+      }
+      
+    });
+    axios.get(`${server.baseURL}/article/myarticlelist`, ).then(data => {
+      if(data.data.msg == "get_article_success") {
+        this.postList2 = data.data.articles
+      }
+      
+    });
+    axios.get(`${server.baseURL}/article/articlelist/2`, ).then(data => {
+      if(data.data.msg == "get_article_success") {
+        this.postList3 = data.data.articles
+      }
+      
+    });
+    axios.get(`${server.baseURL}/article/articlelist/3`, ).then(data => {
+      if(data.data.msg == "get_article_success") {
+        this.postList4 = data.data.articles
+      }
       
     });
 
