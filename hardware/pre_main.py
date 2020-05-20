243b2a3b21d916540acb6c825ca0ad51920910ee
chenyuyuan <1695215206@qq.com>
2020-05-21 00:10:22 +0800

    to hardware

--- hardware/pre_main.py
+++ hardware/pre_main.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     else:
         print("open failed >", serial.name)
     # create a link
-    order_new = "AT+CM2MCLINEW=\"49.4.85.232\",\"5683\",\"868334033365754\"\r\n"
+    order_new = "AT+CM2MCLINEW=\"49.4.85.232\",\"5683\",\"00300001\"\r\n"
     serial.write(order_new.encode())
     order_new_data = serial.read(1)
     time.sleep(0.1)
